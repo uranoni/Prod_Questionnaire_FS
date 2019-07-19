40e3f1a94d0256ec0194c4aa2a8fa85af5e61c6a
Roni <uranoni777@gmail.com>
2019-07-19 16:07:55 +0800

    server-side + Model path

--- server/model/Path.js
+++ server/model/Path.js
@@ -9,10 +9,10 @@ const Path = new mongoose.Schema({
     list_info: { type: Schema.Types.ObjectId, ref: 'List' },
     user_info: { type: Schema.Types.ObjectId, ref: 'User' },
     choose_url: { type: String },
-    choose_option: [
+    choose_item: [
         {
-            option_detail: { type: Schema.Types.ObjectId, ref: 'Option' },
-            option_id: { type: Schema.Types.ObjectId }
+            item_type: { type: Schema.Types.ObjectId },
+            item_id: { type: Schema.Types.ObjectId }
         }
     ]
 
